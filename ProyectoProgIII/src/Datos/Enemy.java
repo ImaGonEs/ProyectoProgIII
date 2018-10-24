3266acccd859f66436a28e561cc2b23076d59013
kikel_000 <kikel_000@PCKike>
2018-10-24 20:20:36 +0200

    q

--- ProyectoProgIII/src/Datos/Enemy.java
+++ ProyectoProgIII/src/Datos/Enemy.java
@@ -6,7 +6,9 @@ import static weareSupports.Clock.*;
 public class Enemy {
 
 	
-	private int x,y,w,h,vel,lp;
+	private int y,w,h,vel,lp;
+	
+	private float x;
 	private Texture tex;
 	private MapCell start;
 	
@@ -19,6 +21,7 @@ public class Enemy {
 	
 	public Enemy(Texture tex, MapCell start, int w, int h, int vel, int lp ) {
 		super();
+		this.start = start;
 		this.x = start.getX();
 		this.y = start.getY();
 		this.w = w;
@@ -36,10 +39,7 @@ public class Enemy {
 		}
 		}
 	
-	public void Draw() {
-		
-		ProjectTQuad(tex, x, y, w, h);	
-	}
+
 	
 	public void Project() {
 		
@@ -48,6 +48,78 @@ public class Enemy {
 		
 		
 		
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getW() {
+		return w;
+	}
+
+	public void setW(int w) {
+		this.w = w;
+	}
+
+	public int getH() {
+		return h;
+	}
+
+	public void setH(int h) {
+		this.h = h;
+	}
+
+	public int getVel() {
+		return vel;
+	}
+
+	public void setVel(int vel) {
+		this.vel = vel;
+	}
+
+	public int getLp() {
+		return lp;
+	}
+
+	public void setLp(int lp) {
+		this.lp = lp;
+	}
+
+	public Texture getTex() {
+		return tex;
+	}
+
+	public void setTex(Texture tex) {
+		this.tex = tex;
+	}
+
+	public MapCell getStart() {
+		return start;
+	}
+
+	public void setStart(MapCell start) {
+		this.start = start;
+	}
+
+	public boolean isFirst() {
+		return first;
+	}
+
+	public void setFirst(boolean first) {
+		this.first = first;
 	}
 	
 	
---
+++ ProyectoProgIII/src/Datos/Wave.java
@@ -0,0 +1,51 @@
+package Datos;
+
+import java.util.ArrayList;
+import static weareSupports.Clock.*;
+public class Wave {
+
+	private int cooldown;
+	private float timeL;
+	private Enemy enemyType;
+	private ArrayList<Enemy> wave;
+	
+	
+	
+	public Wave( int cooldown, Enemy enemyType) {
+		super();
+		
+		this.cooldown = cooldown;
+		this.enemyType = enemyType;
+		timeL =0;
+		wave = new ArrayList<Enemy>();
+	}
+	
+	public void Update() {
+		
+		timeL += Delta();
+		
+		
+		if(timeL>cooldown) {
+			
+			
+			ProjectW();
+			timeL=0;
+			
+			
+		}
+		
+		for (Enemy e : wave) {
+			e.Update();
+			e.Project();
+		}
+	}
+	
+	private void ProjectW() {
+		
+		wave.add(new Enemy(enemyType.getTex(),enemyType.getStart(),32,32,enemyType.getVel(),enemyType.getLp()));
+		
+		
+		
+	}
+	
+}
