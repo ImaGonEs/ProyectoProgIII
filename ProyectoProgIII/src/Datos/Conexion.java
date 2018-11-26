ad2c131a5721afeac1774fd2217049e0bb5ffa14
izai <izai@Izalt>
2018-11-26 01:28:58 +0100

    no llega a conectar con la url, necesita cambio

---
+++ ProyectoProgIII/src/Datos/Conexion.java
@@ -0,0 +1,44 @@
+package Datos;
+
+import java.awt.HeadlessException;
+import java.sql.*;
+
+public class Conexion extends javax.swing.JFrame {
+	
+	public Conexion() {
+		this.setLocationRelativeTo(null);
+				
+	}
+	
+	public static void main(String[] args) {
+		
+		try {
+			String url = "jbdc:mysql://sql7.freesqldatabase.com/sql7267016";
+			String user = "sql7267016";
+			String password = "g4xXgfljeE";	
+			
+			Connection cn = DriverManager.getConnection(url, user, password);
+			PreparedStatement pst = cn.prepareStatement("SELECT * FROM PLAYERS");
+			
+			pst.setString(1, "KIKEXD");
+			
+			ResultSet rs = pst.executeQuery();
+			
+			if (rs.next()) {
+				System.out.println(rs.getString("NAME_P"));
+				System.out.println(rs.getString("PASSWORD"));
+				System.out.println(rs.getString("GEMS"));
+				System.out.println(rs.getString("POS"));
+			}else {
+				System.out.println("REGISTRO NO ENCONTRADO");
+			}
+			
+		} catch (HeadlessException | SQLException e) {
+			
+			System.out.println("Error: " + e);
+			
+		}
+		
+	}
+
+}
