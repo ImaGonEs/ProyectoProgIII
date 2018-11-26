1b1843be98eacc78a7f5c018b16589fe9030f1bc
izai <izai@Izalt>
2018-11-26 17:07:42 +0100

    no estabhlece conexion

--- ProyectoProgIII/src/Datos/Conexion.java
+++ ProyectoProgIII/src/Datos/Conexion.java
@@ -3,42 +3,63 @@ package Datos;
 import java.awt.HeadlessException;
 import java.sql.*;
 
+import javax.swing.JOptionPane;
+
 public class Conexion extends javax.swing.JFrame {
 	
+	public String driver = "com.mysql.cj.jdbc.Driver";
+	public String url = "jbdc:mysql://127.0.0.1/ggtowerdefense";
+	public String user = "ikiggtd";
+	public String password = "elmegapush";
+	Connection cn;
+	Statement stm;
+	
 	public Conexion() {
-		this.setLocationRelativeTo(null);
-				
+		//this.setLocationRelativeTo(null);
+		
+		try {
+			
+			Class.forName(driver);
+			DriverManager.getConnection(url, user, password);
+			JOptionPane.showMessageDialog(null, "Conexion establecida");
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Conexion no establecida con BD" +e.getMessage());
+		}
+		
 	}
 	
 	
 	public static void main(String[] args) {
 		
-		try {
-			String url = "jbdc:mysql://sql7.freesqldatabase.com/sql7267016";
-			String user = "sql7267016";
-			String password = "g4xXgfljeE";	
-			
-			Connection cn = DriverManager.getConnection(url, user, password);
-			PreparedStatement pst = cn.prepareStatement("SELECT * FROM PLAYERS");
-			
-			pst.setString(1, "KIKEXD");
-			
-			ResultSet rs = pst.executeQuery();
-			
-			if (rs.next()) {
-				System.out.println(rs.getString("NAME_P"));
-				System.out.println(rs.getString("PASSWORD"));
-				System.out.println(rs.getString("GEMS"));
-				System.out.println(rs.getString("POS"));
-			}else {
-				System.out.println("REGISTRO NO ENCONTRADO");
-			}
-			
-		} catch (HeadlessException | SQLException e) {
-			
-			System.out.println("Error: " + e);
-			
-		}
+		Conexion c = new Conexion();
+		
+//		try {
+//			public String driver = "com.mysql.jdbc.Driver";
+//			public String url = "jbdc:mysql://127.0.0.1/ggtowerdefense";
+//			public String user = "root";
+//			public String password = "";
+//			
+//			Connection cn = DriverManager.getConnection(url, user, password);
+//			PreparedStatement pst = cn.prepareStatement("SELECT * FROM PLAYERS");
+//			
+//			pst.setString(1, "KIKEXD");
+//			
+//			ResultSet rs = pst.executeQuery();
+//			
+//			if (rs.next()) {
+//				System.out.println(rs.getString("NAME_P"));
+//				System.out.println(rs.getString("PASSWORD"));
+//				System.out.println(rs.getString("GEMS"));
+//				System.out.println(rs.getString("POS"));
+//			}else {
+//				System.out.println("REGISTRO NO ENCONTRADO");
+//			}
+//			
+//		} catch (HeadlessException | SQLException e) {
+//			
+//			System.out.println("Error: " + e);
+//			
+//		}
 		
 	}
 
