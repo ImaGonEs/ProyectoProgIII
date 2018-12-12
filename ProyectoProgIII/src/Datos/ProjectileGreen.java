7bd0fa2885036dbcb2f2d6f3f11f77e7db5d2999
kikel_000 <kikel_000@PCKike>
2018-12-12 18:25:19 +0100

    ahora si

---
+++ ProyectoProgIII/src/Datos/ProjectileGreen.java
@@ -0,0 +1,20 @@
+package Datos;
+
+import org.newdawn.slick.opengl.Texture;
+
+public class ProjectileGreen extends Projectile{
+
+	
+	public ProjectileGreen(Texture tex, Enemy target, float x, float y, int w, int h, float speed, int damage) {
+		super(tex, target, x, y, w, h, speed, damage);
+	}
+
+	@Override
+	public void damage() {
+		
+		
+		super.setAlive(false); //si queremos que no haga daño se cambia esto
+		super.damage();
+	}
+
+}
